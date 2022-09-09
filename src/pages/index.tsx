e097383813ef14aa95da79c07ec37590e2e24451
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-09-09 09:33:24 -0500

    init comit, isntall and configurate Next-Tailwind-TypeScript project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return <div className="text-lg font-medium">Hola neztasdasdasd</div>;
+}
+
+export default Home
