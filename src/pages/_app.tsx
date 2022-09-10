b5d497701b29f9d5d58a4e6e466a49f379f3e615
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-09-10 01:40:50 -0500

    login for stetic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import Head from "next/head";
 
 //Styles
-import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "../../styles/globals.css";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,23 @@ import type { NextPage } from "next";
 
 //Components
 import UserCRUD from "../components/user-crud";
+import Login from "../components/login.jsx";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [login, setLogin] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLogin(false);
+    }, 3000);
+  }, []);
   return (
     <div
-      style={{ height: "95vh" }}
+      style={{ height: "100vh" }}
       className="flex justify-center items-center"
     >
-      <UserCRUD />
+      {login && <Login />}
+      {!login && <UserCRUD />}
     </div>
   );
 };
