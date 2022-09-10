0d4ef902e21e28f841705d6b16a073326567fc35
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-09-10 02:20:51 -0500

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,14 @@ import type { NextPage } from "next";
 
 //Components
 import UserCRUD from "../components/user-crud";
-import Login from "../components/login.jsx";
-import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const [login, setLogin] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLogin(false);
-    }, 3000);
-  }, []);
   return (
     <div
       style={{ height: "100vh" }}
       className="flex justify-center items-center"
     >
-      {login && <Login />}
-      {!login && <UserCRUD />}
+      <UserCRUD />
     </div>
   );
 };
